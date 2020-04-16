f29c365e3411ad199bdabedc935d8966ef729810
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-16 07:02:53 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -39,26 +39,26 @@ end
 
 
 
-
-
-  new_cart=[] #my new, consolidated cart
-  cart.each do |item_full| #checks thru each item of the incoming cart
-    binding.pry
-    item_hash = {}  # reset a temp hash each time thru
-    item_in_cart = "no" #reset thie variable each time thru
-    new_cart.each do |item_check| #checks thru the new_cart to see if item already exists
-      binding.pry
-      if item_full[:item] == item_check[:item]  #checks if the item from cart is already in new cart
-        item_check[:count] += 1 #if yes, we had a count of +1
-        item_in_cart = "yes"
-        binding.pry
-      end
-      if item_in_cart == "no" #if item was not in new cart,
-        item_hash = find_item_by_name_in_collection(item) # we get the item's hash and put in our temp location
-        item_hash[:count] = 1 #give our temp hash, a key of count to be a value of 1
-        new_cart << item_hash #add the items hash onto our new cart hash
-        binding.pry
-      end
-    end
-  end
-end
+# 
+# 
+#   new_cart=[] #my new, consolidated cart
+#   cart.each do |item_full| #checks thru each item of the incoming cart
+#     binding.pry
+#     item_hash = {}  # reset a temp hash each time thru
+#     item_in_cart = "no" #reset thie variable each time thru
+#     new_cart.each do |item_check| #checks thru the new_cart to see if item already exists
+#       binding.pry
+#       if item_full[:item] == item_check[:item]  #checks if the item from cart is already in new cart
+#         item_check[:count] += 1 #if yes, we had a count of +1
+#         item_in_cart = "yes"
+#         binding.pry
+#       end
+#       if item_in_cart == "no" #if item was not in new cart,
+#         item_hash = find_item_by_name_in_collection(item) # we get the item's hash and put in our temp location
+#         item_hash[:count] = 1 #give our temp hash, a key of count to be a value of 1
+#         new_cart << item_hash #add the items hash onto our new cart hash
+#         binding.pry
+#       end
+#     end
+#   end
+# end
